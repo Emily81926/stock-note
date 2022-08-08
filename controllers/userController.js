5a5d2aa5d8d51370f038e5cde73141f510d8f7b3
Emily81926 <emily81926@gmail.com>
2022-08-08 12:18:06 +0800

    refactor: revise login and signin with refresh token feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,13 @@ const bcrypt = require('bcryptjs')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
+const generateAccessToken = (user) => {
+  return jwt.sign({ user_id: user._id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '5s' })
+}
+
+const generateRefreshToken = (user) => {
+  return jwt.sign({ user_id: user._id, email: user.email }, process.env.JWT_REFRESH_SECRET, { expiresIn: '1d' })
+}
 
 
 exports.signUp = async (req, res) => {
@@ -18,13 +25,9 @@ exports.signUp = async (req, res) => {
 
     const hash = await bcrypt.hash(password, 10);
     
-     const token = jwt.sign(
-       { user_id: req.body._id, email },
-       process.env.JWT_SECRET,
-       { expiresIn: '2h' })
 
     const user = await User.create({
-      name, email: email.toLowerCase(), password: hash, token
+      name, email: email.toLowerCase(), password: hash, refreshToken: ''
     })
 
     return res.status(201).json(user)
@@ -33,6 +36,8 @@ exports.signUp = async (req, res) => {
     console.log(error)
   }
 },
+
+
 
   exports.signIn = async (req, res, next) => {
     try {
@@ -44,12 +49,18 @@ exports.signUp = async (req, res) => {
       const user = await User.findOne({ email })
 
       if (user && (await bcrypt.compare(password, user.password))) {
-        const token = jwt.sign({ user_id: user._id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '2h' })
+        const accessToken = generateAccessToken(user);
+        const refreshToken = generateRefreshToken(user);
 
-        user.token = token
-        delete user.password
+        //add refreshToken to db
+        user.refreshToken = refreshToken
+        await user.save()
 
-        return res.status(200).json(user);
+        return res.status(200).json({
+          user: user.name, 
+          email: user.email,
+          accessToken: accessToken
+        });
       }
 
       return res.status(400).send("Invalid Credentials")
