8066ae2d7dcf878b1e9b3dc913962af24bb52a7c
Emily81926 <emily81926@gmail.com>
2022-08-10 19:04:56 +0800

    revise: revise getWatchlist controller, gtom req.body.userId to req.user.user_id

--- controllers/watchlistController.js
+++ controllers/watchlistController.js
@@ -1,28 +1,29 @@
 const watchList = require('../models/watchlist')
 
-exports.postWatchlist = async(req, res) => {
-  try { 
-    const { symbol, companyName, sector, price, userId} = req.body 
+exports.postWatchlist = async (req, res) => {
+  try {
+    const { symbol, companyName, sector, price, userId } = req.body
     await watchList.create({
       symbol, companyName, sector, price, userId
     })
     return res.status(200).json('successfully added to watchlist!');
-  }catch(error){
+  } catch (error) {
     console.log(error)
-  } 
+  }
 }
 
-exports.getWatchlist = async(req, res) => {
-  const {userId} = req.body
-  const list = await watchList.find({userId})
+exports.getWatchlist = async (req, res) => {
+  //axios.get不能接收到req.body，因此沒辦法用const userId = req.body.userId
+  const userId = req.user.user_id
+  const list = await watchList.find({ userId })
 
-  return res.status(200).json({list})
+  return res.status(200).json(list)
 }
 
 
-exports.deleteWatchlist = async(req, res) => {
+exports.deleteWatchlist = async (req, res) => {
   const { watchListId } = req.body
   await watchList.findByIdAndDelete(watchListId)
 
-return res.status(200).json('successfully deleted from watchlist!')
+  return res.status(200).json('successfully deleted from watchlist!')
 }
