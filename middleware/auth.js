54c0b96d39c970080667c7d4e430c8d52911df3c
Emily81926 <emily81926@gmail.com>
2022-12-06 19:14:01 +0800

    revise: revise middleware/auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -14,19 +14,6 @@ const verifyToken = (req, res, next) => {
   }else{
     res.status(401).json("You are not authenticated!!")
   }
-  // const token = req.body.token || req.query.token || req.headers["x-access-token"];
-  
-  // if(!token){
-  //   return res.status(403).send("A token is required for authentication")
-  // }
-
-  // try{
-  //   const decoded = jwt.verify(token, process.env.JWT_SECRET)
-  //   req.user = decoded
-  // }catch(error){ 
-  //   return res.status(401).send("Invalid token");
-  // }
-  //return next();
 }
 
 
