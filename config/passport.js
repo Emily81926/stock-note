4f0083169bcd3fb8673adb15a39d3a73900a9cca
Emily81926 <emily81926@gmail.com>
2023-01-29 02:32:40 +0800

    revise: revise passport google strategy accesstoken setting

--- config/passport.js
+++ config/passport.js
@@ -4,9 +4,9 @@ const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 
-// const generateAccessToken = (user) => {
-//   return jwt.sign({ user_id: user._id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '15m' })
-// }
+const generateAccessToken = (user) => {
+  return jwt.sign({ user_id: user._id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '15m' })
+}
 
 const generateRefreshToken = (user) => {
   return jwt.sign({ user_id: user._id, email: user.email }, process.env.JWT_REFRESH_SECRET, { expiresIn: '1d' })
@@ -20,34 +20,32 @@ passport.use(new GoogleStrategy({
   callbackURL: "/auth/google/callback",
   passReqToCallback: true
 }, async function (req, accessToken, refreshToken, profile, callback) {
-  console.log('google strategy')
-  console.log(profile._json)
   //看是否信箱已註冊過，有註冊過就註冊access token跟 refresh token，並回傳原本的資料
   const { email, name } = profile._json
   const oldUser = await User.findOne({ email })
-  console.log("oldUser: ", oldUser)
   if (oldUser) {
-    console.log("find old user!!")
     //找到user後，分發access token跟 refresh token
-    //const accessToken = generateAccessToken(oldUser)
+    const accessToken = generateAccessToken(oldUser)
 
     const refreshToken = generateRefreshToken(oldUser)
     //將refresh token存進db
     oldUser.refreshToken = refreshToken
     await oldUser.save()
 
-    //加這個看會不會req.user變成我要的檔案
-    //const data = Object.assign(oldUser, accessToken) //合併oldUser 跟access token這兩個object
-    //req.user = data
 
-    return callback(null, oldUser)
+    return callback(null, {
+      name,
+      email,
+      accessToken: accessToken,
+      refreshToken: refreshToken
+    })
   }
 
   //沒有的話，就新註冊
   const newAccessToken = jwt.sign(
     { email },
     process.env.JWT_SECRET,
-    { expiresIn: '5m' })
+    { expiresIn: '15m' })
 
   const newRefreshToken = jwt.sign(
     { email },
@@ -70,13 +68,11 @@ passport.use(new GoogleStrategy({
 
 
 passport.serializeUser((user, done) => {
-  console.log('passport serializeUser', user)
   done(null, user)
   
 })
 
 passport.deserializeUser((user, done) => {
-  console.log('passport deserializeUser', user)
   done(null, user)
 })
 
